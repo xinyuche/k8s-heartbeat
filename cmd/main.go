be9bb7b6e13bb06d961188b3e3730f31a16db793
xinyuche <xinyuche@buffalo.edu>
2021-11-03 16:34:30 -0400

    add Dockerfile and add some log

--- cmd/main.go
+++ cmd/main.go
@@ -11,20 +11,25 @@ import (
 )
 
 func main() {
+	log.Println("Starting to find InClusterConfig")
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		log.Println("Failed to get InClusterConfig")
+	} else {
+		log.Println("Find InClusterConfig successful")
 	}
 	clientSet, err := kubernetes.NewForConfig(config)
 
 	// loop the heartbeat style test for every 5 min
 	for {
+		log.Println("Starting to fetch node list")
 		nodelist, err := clientSet.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
 			log.Println("Failed to get node list")
 		}
 		numOfNode := len(nodelist.Items)
 		log.Printf("current num of node: %v", numOfNode)
+		log.Println("Cycle over, sleep for 5 mins")
 		time.Sleep(5 * time.Minute)
 	}
 }
